f5eca485689cc0b11d54a8151cb16d819cd91eca
r0cketman <fernando@zavalasystems.com>
2016-02-24 13:38:43 -0600

    added config directory, and stubbed post-docker-process script

---
+++ configs/post-docker-process.sh
@@ -0,0 +1,9 @@
+################################################################################
+# Author:   Fernando Zavala
+# Company:  Pivotal Software Inc.
+# Date:     2/24/2016
+# Purpose:  Dockerizing Pivotal Gemfire
+#           This scripts tests docker container after it's been created and started.
+#
+#
+################################################################################
